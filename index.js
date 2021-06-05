49716a78e8a56721d2ef4878c0910f668d637499
ilya22070 <62645091+ilya22070@users.noreply.github.com>
2021-06-05 22:52:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 let popupBtn = document.querySelector('.price__left_btn_item'),
-  popup = document.querySelector('.popup')
+  popup = document.querySelector('.popup'),
+  burger = document.querySelector('.header__nav')
+
+let navLinks = document.querySelectorAll('.header__nav_item')
+
+navLinks.forEach((e) => {
+  let elementID = e.dataset.scroll
+
+  e.addEventListener('click', function (e) {
+    let elementOffset = document.querySelector(elementID).offsetTop
+    e.preventDefault()
+    window.scrollTo({
+      top: elementOffset - 150,
+      behavior: 'smooth',
+    })
+    if (burger.classList.contains('header__nav_burger')) {
+      burger.classList.remove('header__nav_burger')
+    }
+  })
+})
 
 window.addEventListener('click', (e) => {
   if (e.target.closest('.price__left_btn_item')) {
@@ -7,7 +26,16 @@ window.addEventListener('click', (e) => {
     popup.classList.add('popup__active')
     return
   }
-  if (!e.target.closest('.popup')) {
+  if (
+    !e.target.closest('.popup') &&
+    popup.classList.contains('popup__active')
+  ) {
     popup.classList.remove('popup__active')
+  }
+  if (e.target.closest('.burger')) {
+    burger.classList.add('header__nav_burger')
+  }
+  if (e.target.closest('.burger__close')) {
+    burger.classList.remove('header__nav_burger')
   }
 })
