36274aa8aa85eeab849da56d398782af1ba0fa6d
ilya2207 <gergert.ilya@mail.ru>
2022-02-12 16:10:23 +0300

    init heroku files

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ window.addEventListener('click', (e) => {
     popup.classList.add('popup__active')
     return
   }
-  console.log(e.target.closest('.popup__close'))
   if (
     (!e.target.closest('.popup') &&
       popup.classList.contains('popup__active')) ||
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$a = 3;
+if ($a === 3) {
+    echo $a;
+}
+
+
+
+echo 'Hello world';
+
+echo '<a href="./index.php">To site</a>';
